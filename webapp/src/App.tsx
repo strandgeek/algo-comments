215f1b6af0c3a7ef22aad0a1350ccfa511476409
strandgeek <strandgeek@gmail.com>
2022-10-29 21:22:23 -0400

    chore(webapp): add initial webapp structure

---
+++ webapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Algorand!
+      <button className="btn btn-primary">
+        Hello
+      </button>
+    </div>
+  );
+}
+
+export default App;
